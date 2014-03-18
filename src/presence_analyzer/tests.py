c14f5b4865c34d0bb0dc6586e77d699317c330ed
Exorades <exorades@gmail.com>
2014-03-18 11:19:55 +0100

    [1527] Dodane brakujace testy, gotowy do rewiew

--- src/presence_analyzer/tests.py
+++ src/presence_analyzer/tests.py
@@ -84,6 +84,97 @@ class PresenceAnalyzerUtilsTestCase(unittest.TestCase):
         self.assertEqual(data[10][sample_date]['start'],
                          datetime.time(9, 39, 5))
 
+    def test_group_by_weekday(self):
+        """
+        Testing groups presence entries by weekday
+        """
+        import_data = utils.get_data()
+        user_10 = utils.group_by_weekday(import_data[10])
+        self.assertEqual(user_10.keys(), range(7))
+        self.assertEqual(user_10[0], [])
+        self.assertEqual(user_10[1], [30047])
+        self.assertEqual(user_10[2], [24465])
+        self.assertEqual(user_10[3], [23705])
+        self.assertEqual(user_10[4], [])
+        self.assertEqual(user_10[5], [])
+        self.assertEqual(user_10[6], [])
+
+        user_11 = utils.group_by_weekday(import_data[11])
+        self.assertEqual(user_11.keys(), range(7))
+        self.assertEqual(user_11[0], [24123])
+        self.assertEqual(user_11[1], [16564])
+        self.assertEqual(user_11[2], [25321])
+        self.assertEqual(user_11[3], [22969, 22999])
+        self.assertEqual(user_11[4], [6426])
+        self.assertEqual(user_11[5], [])
+        self.assertEqual(user_11[6], [])
+
+    def test_seconds_since_midnight(self):
+        """
+        Test Calculations amount of seconds since midnight.
+        """
+        data = utils.seconds_since_midnight
+        self.assertEqual(data(
+            datetime.time(0, 1, 0)), 60)
+        self.assertEqual(data(
+            datetime.time(0, 10, 0)), 600)
+        self.assertEqual(data(
+            datetime.time(0, 30, 30)), 1830)
+        self.assertEqual(data(
+            datetime.time(1, 30, 30)), 5430)
+        self.assertEqual(data(
+            datetime.time(3, 0, 0,)), 10800)
+        self.assertEqual(data(
+            datetime.time(5, 30, 0)), 19800)
+        self.assertEqual(data(
+            datetime.time(10, 0, 0)), 36000)
+        self.assertEqual(data(
+            datetime.time(23, 0, 0)), 82800)
+
+    def test_interval(self):
+        """
+        Test calculations inverval in seconds between two
+        datetime.time objects.
+        """
+        data = utils.interval
+        self.assertEqual(data(
+            datetime.time(02, 00, 00),
+            datetime.time(02, 00, 50)), 50)
+        self.assertEqual(data(
+            datetime.time(02, 00, 00),
+            datetime.time(01, 58, 43)), -77)
+        self.assertEqual(data(
+            datetime.time(10, 30, 00),
+            datetime.time(11, 00, 00)), 1800)
+        self.assertEqual(data(
+            datetime.time(15, 00, 00),
+            datetime.time(14, 30, 00)), -1800)
+        self.assertEqual(data(
+            datetime.time(10, 00, 00),
+            datetime.time(11, 00, 00)), 3600)
+        self.assertEqual(data(
+            datetime.time(18, 00, 00),
+            datetime.time(17, 00, 00)), -3600)
+        self.assertEqual(data(
+            datetime.time(00, 00, 00),
+            datetime.time(10, 00, 00)), 36000)
+        self.assertEqual(data(
+            datetime.time(12, 00, 00),
+            datetime.time(05, 00, 00)), -25200)
+
+    def test_mean(self):
+        """
+        Test calculations of arithmetic mean. Returns zero for empty lists.
+        float(sum(items)) / len(items) if len(items) > 0 else 0
+        """
+        dane = utils.mean
+        self.assertEqual(dane([0, 0, 0]), 0)
+        self.assertEqual(dane([0, 10]), 5)
+        self.assertEqual(dane([10, 10]), 10)
+        self.assertEqual(dane([10, 20, 80, 100]), 52.5)
+        self.assertEqual(dane([26000, 36000, 1000]), 21000)
+        self.assertEqual(dane([25200, 3600, 1800, 100]), 7675)
+
 
 def suite():
     """
